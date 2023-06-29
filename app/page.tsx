992b9e394439d27c1f6bffb751e9b3c8ced01fd3
samueloh99 <samueloh99@gmail.com>
2023-06-29 16:46:51 -0300

    Change NextImage NextLink names and change a tag to p tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import NextImage from "next/image"
-import NextLink from "next/link"
+import Image from "next/image"
+import Link from "next/link"
 import PSELogo from "@/public/icons/archstar.webp"
 import ArrowRightVector from "@/public/icons/arrow-right.svg"
 import PSEIcon1 from "@/public/icons/pseicon1.svg"
@@ -21,24 +21,20 @@ export default function IndexPage() {
           <h1 className="text-4xl font-[700] lg:text-7xl">
             Programmable cryptography for people like you
           </h1>
-          <NextLink href={"/projects"} className="flex items-center gap-2">
+          <Link href={"/projects"} className="flex items-center gap-2">
             <h1 className="border-b-2 border-orange text-base uppercase">
               Explore The Project Library
             </h1>
-            <NextImage
+            <Image
               src={ArrowRightVector}
               alt="arrowvector"
               height={24}
               width={24}
             />
-          </NextLink>
+          </Link>
         </div>
         <div className="m-auto flex h-[364px] w-full max-w-[280px] items-center justify-center md:m-0 md:h-full md:w-full lg:max-w-[542px]">
-          <NextImage
-            src={PSELogo}
-            alt="pselogo"
-            style={{ objectFit: "cover" }}
-          />
+          <Image src={PSELogo} alt="pselogo" style={{ objectFit: "cover" }} />
         </div>
       </div>
 
@@ -46,7 +42,7 @@ export default function IndexPage() {
         <div className="flex flex-col justify-between gap-5 border-y border-black px-[32px] py-5 md:flex-row md:gap-0 lg:px-20">
           <div className="flex flex-col items-start justify-center gap-2 lg:flex-row lg:items-center">
             <div className="flex w-[280px] items-center justify-start gap-2">
-              <NextImage src={PSEIcon1} alt="pseicon1" height={16} width={16} />
+              <Image src={PSEIcon1} alt="pseicon1" height={16} width={16} />
               <p className="text-base font-[700] uppercase tracking-wide text-orange">
                 Learn and Share
               </p>
@@ -55,7 +51,7 @@ export default function IndexPage() {
               Diving into Plonk accumulation via aPlonk by Ralph Toledo
             </p>
           </div>
-          <NextLink
+          <Link
             href={"https://www.youtube.com/live/hRXgf6T2yb8?feature=share"}
             target="_blank"
             rel="noreferrer"
@@ -65,12 +61,12 @@ export default function IndexPage() {
               Watch
             </p>
             <ArrowRightUp color="black" />
-          </NextLink>
+          </Link>
         </div>
         <div className="flex flex-col justify-between gap-5 border-b border-black px-[32px] py-5  md:flex-row md:gap-0 lg:px-20">
           <div className="flex flex-col items-start justify-center gap-2 lg:flex-row lg:items-center">
             <div className="flex w-[280px] items-center justify-start gap-2">
-              <NextImage src={PSEIcon1} alt="pseicon1" height={16} width={16} />
+              <Image src={PSEIcon1} alt="pseicon1" height={16} width={16} />
               <p className="text-base font-[700] uppercase tracking-wide text-orange">
                 Learn and Share
               </p>
@@ -79,7 +75,7 @@ export default function IndexPage() {
               Folding Circom Circuit: A ZKML Case Study - Dr. Cathie So
             </p>
           </div>
-          <NextLink
+          <Link
             href={"https://www.youtube.com/live/jb6HDEtY4CI?feature=share"}
             target="_blank"
             rel="noreferrer"
@@ -89,19 +85,19 @@ export default function IndexPage() {
               Watch
             </p>
             <ArrowRightUp color="black" />
-          </NextLink>
+          </Link>
         </div>
         <div className="flex flex-col justify-between gap-5 border-b border-black px-[32px] py-5 md:flex-row md:gap-0 lg:px-20">
           <div className="flex flex-col items-start justify-center gap-2 lg:flex-row lg:items-center">
             <div className="flex w-[280px] items-center justify-start gap-2">
-              <NextImage src={PSEIcon2} alt="pseicon1" height={16} width={16} />
+              <Image src={PSEIcon2} alt="pseicon1" height={16} width={16} />
               <p className="text-base font-[700] uppercase tracking-wide text-orange">
                 Event
               </p>
             </div>
             <p className="text-xl font-[500]">PSE @ETH Global Paris</p>
           </div>
-          <NextLink
+          <Link
             href={"https://ethglobal.com/events/paris2023"}
             target="_blank"
             rel="noreferrer"
@@ -111,12 +107,12 @@ export default function IndexPage() {
               Attend
             </p>
             <ArrowRightUp color="black" />
-          </NextLink>
+          </Link>
         </div>
         <div className="flex flex-col justify-between gap-5 border-b border-black px-[32px] py-5 md:flex-row md:gap-0 lg:px-20">
           <div className="flex flex-col items-start justify-center gap-2 lg:flex-row lg:items-center">
             <div className="flex w-[280px] items-center justify-start gap-2">
-              <NextImage src={PSEIcon3} alt="pseicon1" height={16} width={16} />
+              <Image src={PSEIcon3} alt="pseicon1" height={16} width={16} />
               <p className="text-base font-[700] uppercase tracking-wide text-orange">
                 Blog Post
               </p>
@@ -125,7 +121,7 @@ export default function IndexPage() {
               zkEVM Community Edition Part 3: Logic and Structure
             </p>
           </div>
-          <NextLink
+          <Link
             href={
               "https://mirror.xyz/privacy-scaling-explorations.eth/shl8eMBiObd6_AUBikXZrjKD4fibI6xUZd7d9Yv5ezE"
             }
@@ -134,11 +130,11 @@ export default function IndexPage() {
             rel="noreferrer"
             className="flex items-center justify-start gap-5 md:justify-center"
           >
-            <a className="border-b-2 border-orange text-base font-[500] uppercase">
+            <p className="border-b-2 border-orange text-base font-[500] uppercase">
               read
-            </a>
+            </p>
             <ArrowRightUp color="black" />
-          </NextLink>
+          </Link>
         </div>
       </div>
 
@@ -169,7 +165,7 @@ export default function IndexPage() {
               contribute to projects, or join our team. We’d love to hear from
               you!
             </h1>
-            <NextLink
+            <Link
               href={siteConfig.links.discord}
               target="_blank"
               rel="noreferrer"
@@ -179,7 +175,7 @@ export default function IndexPage() {
                 Say Hi On Discord
               </h1>
               <ArrowRightUp color="black" />
-            </NextLink>
+            </Link>
           </div>
         </section>
       </div>
