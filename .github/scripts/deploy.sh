f90daf85fd7059ddce00644d4f7a12f03eceb532
Nick <nick@gmx.co.uk>
2023-07-07 10:00:36 +0300

    Fix var name

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -3,8 +3,8 @@ set -ex
  
 tasks="pse-web"
 for task in $tasks; do
-  pse-web_revision=$(aws ecs describe-task-definition --task-definition $task --query "taskDefinition.revision")
-  aws ecs update-service --cluster pse-web --service $task --force-new-deployment --task-definition $task:$pse-web_revision
+  pse_web_revision=$(aws ecs describe-task-definition --task-definition $task --query "taskDefinition.revision")
+  aws ecs update-service --cluster pse-web --service $task --force-new-deployment --task-definition $task:$pse_web_revision
 done
 
 aws ecs wait services-stable --cluster pse-web --services $tasks
