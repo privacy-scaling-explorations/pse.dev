4d16a401dfba91080eddf738d6448eeb77d4d526
Nick <nick@gmx.co.uk>
2023-07-11 17:16:38 +0300

    Change deploy workflow + aux scripts to support multiple ENVs

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 build=$1
+env=$2
 
 [ $build = "enable" ] || exit 0
 
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 490752553772.dkr.ecr.eu-central-1.amazonaws.com
 
 docker build -t pse-web .
-docker tag pse-web:latest 490752553772.dkr.ecr.eu-central-1.amazonaws.com/pse-web:latest
-docker push 490752553772.dkr.ecr.eu-central-1.amazonaws.com/pse-web:latest
+docker tag pse-web:latest 490752553772.dkr.ecr.eu-central-1.amazonaws.com/pse-web-$env:latest
+docker push 490752553772.dkr.ecr.eu-central-1.amazonaws.com/pse-web-$env:latest
 
 exit 0
--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -ex
+
+env=$1
  
-tasks="pse-web"
+tasks="pse-web-$env"
 for task in $tasks; do
   pse_web_revision=$(aws ecs describe-task-definition --task-definition $task --query "taskDefinition.revision")
-  aws ecs update-service --cluster pse-web --service $task --force-new-deployment --task-definition $task:$pse_web_revision
+  aws ecs update-service --cluster pse-web-$env --service $task --force-new-deployment --task-definition $task:$pse_web_revision
 done
 
-aws ecs wait services-stable --cluster pse-web --services $tasks
+aws ecs wait services-stable --cluster pse-web-$env --services $tasks
