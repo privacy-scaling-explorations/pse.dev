d912f1cd37dc73aa7eb78c5eec5b80a5e864b3de
Nick <nick@gmx.co.uk>
2023-07-12 09:33:05 +0300

    Troubleshooting url for stg

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -11,13 +11,9 @@ env=$1
 #
 #aws ecs wait services-stable --cluster pse-web-$env --services $tasks
 
-echo "Before"
-
-#if [ $env = "stg" ]; then
+if [ $env = "stg" ]; then
   task_arn=$(aws ecs list-tasks --cluster pse-web-$env --query 'taskArns[]' --output text)
   eni_id=$(aws ecs describe-tasks --cluster pse-web-$env --task $task_arn --query 'tasks[].attachments[].details[?name==`networkInterfaceId`].value' --output text)
   public_ip=$(aws ec2 describe-network-interfaces --network-interface-ids $eni_id --query 'NetworkInterfaces[].Association.PublicIp' --output text) 
   echo "STG Url: http://$public_ip:3000"
-#fi
-
-echo "After"
+fi
