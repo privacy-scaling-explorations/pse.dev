8b7ee4e97164bc08f56b08e672e83ef372a33b2f
Nick <nick@gmx.co.uk>
2023-07-12 09:05:29 +0300

    Show Url for stg env

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -10,3 +10,10 @@ for task in $tasks; do
 done
 
 aws ecs wait services-stable --cluster pse-web-$env --services $tasks
+
+if [ $env = "stg" ]; then
+  task_arn=$(aws ecs list-tasks --cluster pse-web-$env --query 'taskArns[]' --output text)
+  eni_id=$(aws ecs describe-tasks --cluster pse-web-$env --task $task_arn --query 'tasks[].attachments[].details[?name==`networkInterfaceId`].value' --output text)
+  public_ip=$(aws ec2 describe-network-interfaces --network-interface-ids $eni_id --query 'NetworkInterfaces[].Association.PublicIp' --output text) 
+  echo "STG Url: http://$public_ip:3000"
+fi
