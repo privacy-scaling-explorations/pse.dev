08e1098945f1a4e40c914aab05608a2b9f1d16e5
Nick <nick@gmx.co.uk>
2023-07-12 09:05:00 +0300

    Change push commands

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -7,8 +7,8 @@ env=$2
 
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 490752553772.dkr.ecr.eu-central-1.amazonaws.com
 
-docker build -t pse-web .
-docker tag pse-web:latest 490752553772.dkr.ecr.eu-central-1.amazonaws.com/pse-web-$env:latest
+docker build -t pse-web-$env .
+docker tag pse-web-$env:latest 490752553772.dkr.ecr.eu-central-1.amazonaws.com/pse-web-$env:latest
 docker push 490752553772.dkr.ecr.eu-central-1.amazonaws.com/pse-web-$env:latest
 
 exit 0
