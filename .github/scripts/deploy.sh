718ec3807385482fac15a9f8aa53acc3f3c20ee7
Nick <nick@gmx.co.uk>
2023-07-12 09:30:48 +0300

    Troubleshooting url for stg

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -3,13 +3,13 @@ set -ex
 
 env=$1
  
-tasks="pse-web-$env"
-for task in $tasks; do
-  pse_web_revision=$(aws ecs describe-task-definition --task-definition $task --query "taskDefinition.revision")
-  aws ecs update-service --cluster pse-web-$env --service $task --force-new-deployment --task-definition $task:$pse_web_revision
-done
-
-aws ecs wait services-stable --cluster pse-web-$env --services $tasks
+#tasks="pse-web-$env"
+#for task in $tasks; do
+#  pse_web_revision=$(aws ecs describe-task-definition --task-definition $task --query "taskDefinition.revision")
+#  aws ecs update-service --cluster pse-web-$env --service $task --force-new-deployment --task-definition $task:$pse_web_revision
+#done
+#
+#aws ecs wait services-stable --cluster pse-web-$env --services $tasks
 
 echo "Before"
 
