40239cc97b99e119c20a0cb09b05e94efe1477b6
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-07-14 15:11:16 -0500

    fix: :pencil2: Fix minor typo on new item

--- data/news.ts
+++ data/news.ts
@@ -11,7 +11,7 @@ export const newsItems: NewsInterface[] = [
   },
   {
     type: "post",
-    title: " Learnings from the KZG Ceremony",
+    title: "Learnings from the KZG Ceremony",
     action: {
       label: "Read",
       url: "https://mirror.xyz/privacy-scaling-explorations.eth/naTdx-u7kyirczTLSAnWwH6ZdedfTQu1yCWQj1m_n-E",
@@ -33,4 +33,4 @@ export const newsItems: NewsInterface[] = [
       url: "https://ethglobal.com/events/paris2023",
     },
   },
-] 
+]
