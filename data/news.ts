8ab7294b8f22769bff12ae730f16056c93899df6
Y6NDR <19380973+thebeyondr@users.noreply.github.com>
2023-08-04 09:20:08 -0500

    Update news.ts
    Update verbs for action labels

--- data/news.ts
+++ data/news.ts
@@ -13,7 +13,7 @@ export const newsItems: NewsInterface[] = [
     type: "learn",
     title: "Revisiting Nova. How to handle cycles of curves within folding schemes by Wilson Nguyen. Aug/9, 16:00 UTC",
     action: {
-      label: "Detail",
+      label: "See details",
       url: "https://discord.com/events/943612659163602974/1128713844987002984",
     },
   },
@@ -21,7 +21,7 @@ export const newsItems: NewsInterface[] = [
     type: "learn",
     title: "Diving into Indexed Merkle Trees within ZK circuits by Sean Aztec. Aug/16, 12:00 UTC ",
     action: {
-      label: "Detail",
+      label: "See details",
       url: "https://discord.gg/pse?event=1137001447679070318",
     },
   },
