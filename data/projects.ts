066db40f857de2961a66965e350cf1d1d9b25f0d
sripwoud <me@sripwoud.xyz>
2023-08-05 12:24:55 +0200

    Update anonklub project data entry

--- data/projects.ts
+++ data/projects.ts
@@ -334,14 +334,14 @@ export const projects = [
 
   {
     id: "anon-klub",
-    image: "",
+    image: "anonklub.svg",
     name: "AnonKlub",
     tldr: "A mechanism for anonymous proof of Ethereum address ownership.",
     description:
       "AnonKlub is a tool designed for Ethereum developers that allows for anonymous proof of Ethereum address ownership. It doesn't directly address the public observability of Ethereum transactions but provides a workaround for privacy. Users can prepare a list of Ethereum addresses, sign a message from an address they own, and use that signature to generate a zero-knowledge proof. This proof enables users to perform actions anonymously that would typically require ownership of an address from the initial list. Use cases include anonymous NFT minting and Discord verification for DAOs without disclosing the public address.",
     links: {
-      github: "https://github.com/privacy-scaling-explorations/e2e-zk-ecdsa",
-      website: "https://anonklub.fly.dev/",
+      github: "https://github.com/anonklub",
+      website: "https://anonklub.github.io",
     },
     tags: {
       themes: [
