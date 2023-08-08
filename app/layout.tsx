eab3a78637137c2177a8ed89e94ff86cd7b646e5
Kalidou Diagne <kld.diagne@gmail.com>
2023-08-08 15:52:03 +0200

    insert matomo analytic to the site

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css"
 import { Metadata } from "next"
+import Script from "next/script"
 
 import { siteConfig } from "@/config/site"
 import { fontDisplay, fontSans } from "@/lib/fonts"
@@ -39,9 +40,24 @@ export default function RootLayout({ children }: RootLayoutProps) {
         className={`${fontSans.variable} ${fontDisplay.variable}`}
         suppressHydrationWarning
       >
+        <Script id="matomo-tracking" strategy="afterInteractive">
+          {`
+             var _paq = window._paq = window._paq || [];
+             /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+             _paq.push(['trackPageView']);
+             _paq.push(['enableLinkTracking']);
+             (function() {
+               var u="https://psedev.matomo.cloud/";
+               _paq.push(['setTrackerUrl', u+'matomo.php']);
+               _paq.push(['setSiteId', '1']);
+               var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+               g.async=true; g.src='//cdn.matomo.cloud/psedev.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+             })();
+          `}
+        </Script>
         <head />
         <body className={"min-h-screen bg-background antialiased"}>
-          <div className="relative flex min-h-screen flex-col">
+          <div className="relative flex flex-col min-h-screen">
             <SiteHeader />
             <div className="flex-1">{children}</div>
             <SiteFooter />
