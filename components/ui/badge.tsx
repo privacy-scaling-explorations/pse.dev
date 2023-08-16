97218423ee0dec42dcac35d92f16d89712b2d1af
Kalidou Diagne <kld.diagne@gmail.com>
2023-08-16 21:21:29 +0200

    add badge component

---
+++ components/ui/badge.tsx
@@ -0,0 +1,18 @@
+import React, { HtmlHTMLAttributes } from "react"
+
+interface BadgeProps extends HtmlHTMLAttributes<HTMLDivElement> {
+  value?: number | string
+}
+
+export default function Badge({ value, children }: BadgeProps) {
+  if (!value) return <>{children}</>
+
+  return (
+    <div className="relative">
+      <div className="absolute flex items-center justify-center min-w-4 h-4 m-auto rounded-full bg-anakiwa-950  -top-[5px] -right-[5px] px-1">
+        <span className="text-xs text-white ">{value}</span>
+      </div>
+      {children}
+    </div>
+  )
+}
