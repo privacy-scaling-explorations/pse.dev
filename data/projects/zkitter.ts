d8555e8eaa13baf75b3c65662b452261fadae35f
Kalidou Diagne <kld.diagne@gmail.com>
2023-08-17 15:31:30 +0200

    make zkitter archived

--- data/projects/zkitter.ts
+++ data/projects/zkitter.ts
@@ -6,7 +6,7 @@ Zkitter is a decentralized social network that emphasizes privacy by default. It
 
 export const zkitter: ProjectInterface = {
   id: "zkitter",
-  projectStatus: "active",
+  projectStatus: "archived",
   image: "zkitter.webp",
   name: "Zkitter",
   tldr: "A decentralized social network prioritizing privacy and anonymity",
