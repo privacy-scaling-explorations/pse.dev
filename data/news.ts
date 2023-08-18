637c1413a9ec31d5e78db2c7c3ac8a0ea86de80e
Chiali <info@chialitsai.com>
2023-08-18 09:10:14 -0400

    capitalize the PARC

--- data/news.ts
+++ data/news.ts
@@ -11,7 +11,7 @@ export const newsItems: NewsInterface[] = [
   },
   {
     type: "learn",
-    title: "Private markets on Ethereum - 0xParc Piere. August 23, 2023 14:00 (UTC)",
+    title: "Private markets on Ethereum - 0xPARC Piere. August 23, 2023 14:00 (UTC)",
     action: {
       label: "See details",
       url: "https://discord.com/events/943612659163602974/1139573494372388914",
