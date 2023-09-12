c384c788d51c9cd3fe267d8b6e03f4e03b430c0d
Y6NDR <19380973+thebeyondr@users.noreply.github.com>
2023-09-12 13:33:23 -0500

    Add powers-of-tau.ts

---
+++ data/projects/powers-of-tau.ts
@@ -0,0 +1,22 @@
+export const PowersOfTau: ProjectInterface = {
+  id: "powers-of-tau",
+  image: "",
+  name: "Perpetual Powers of Tau",
+  tags: {
+    themes: ["play", "buildWith"],
+  },
+  extraLinks: {
+    buildWith: [
+      {
+        label: "Link to get started",
+        url: 'https://google.it"',
+      },
+    ],
+    play: [
+      {
+        label: "Link to get started",
+        url: 'https://google.it"',
+      },
+    ],
+  },
+}
