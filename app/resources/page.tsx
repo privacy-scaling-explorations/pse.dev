b0747412549875d797f261a112ab9cfd1f516169
Kalidou Diagne <kld.diagne@gmail.com>
2023-09-12 18:47:19 +0100

    update resource nav scroll

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -1,8 +1,9 @@
 "use client"
 
-import React from "react"
+import React, { useEffect } from "react"
 import Link from "next/link"
 
+import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
 
@@ -24,12 +25,6 @@ interface ResourceCardProps {
   id?: string
   title: string
   children?: React.ReactNode
-}
-
-interface ResourceNavItemProps {
-  id: string
-  children: React.ReactNode
-  active?: boolean
 }
 
 const ResourceItem = ({
@@ -70,6 +65,7 @@ const ResourceCard = ({ id, title, children }: ResourceCardProps) => {
   return (
     <div
       id={id}
+      data-section={id}
       className="flex flex-col gap-4 p-4"
       style={{
         background: "rgba(255, 255, 255, 0.30",
@@ -83,52 +79,75 @@ const ResourceCard = ({ id, title, children }: ResourceCardProps) => {
   )
 }
 
-const ResourceNavItem = ({
-  id,
-  children,
-  active = false,
-}: ResourceNavItemProps) => {
-  const onScroll = () => {
+const ResourceNav = () => {
+  const SCROLL_OFFSET = 80
+  const [activeId, setActiveId] = React.useState("")
+  const ID_LABELS_MAPPING: Record<string, string> = {
+    "get-involved": "Get involved",
+    learn: "Learn",
+    build: "Build",
+    design: "Design",
+  }
+
+  useEffect(() => {
+    const sections: any = document.querySelectorAll(`div[data-section]`)
+
+    window.onscroll = () => {
+      sections.forEach((section: any) => {
+        const sectionTop = section.offsetTop - SCROLL_OFFSET
+        if (window.scrollY >= sectionTop && window.scrollY > 0) {
+          setActiveId(section.getAttribute("id"))
+        }
+      })
+    }
+  }, [])
+
+  const scrollToId = (id: string) => {
     const element = document.getElementById(id)
     const top = element?.offsetTop ?? 0
-    const offset = 80
 
     if (element) {
       window?.scrollTo({
         behavior: "smooth",
-        top: (top ?? 0) - offset,
+        top: (top ?? 0) - SCROLL_OFFSET,
       })
+      setActiveId(id) // active clicked id
     }
   }
 
-  return (
-    <li
-      onClick={onScroll}
-      className="flex items-center h-8 px-3 border-l-2 cursor-pointer"
-    >
-      {children}
-    </li>
-  )
-}
-
-const ResourceNav = () => {
   return (
     <div className="flex flex-col gap-6 p-8">
       <div className="flex flex-col gap-4">
         <span className="text-lg font-bold text-tuatara-700 font-display">
           On this page
         </span>
-        <ul className="font-sans text-black border-l-2 text-normal">
-          <ResourceNavItem id="get-involved">Get involved</ResourceNavItem>
-          <ResourceNavItem id="learn">Learn</ResourceNavItem>
-          <ResourceNavItem id="build">Build</ResourceNavItem>
-          <ResourceNavItem id="design">Design</ResourceNavItem>
+        <ul className="font-sans text-black text-normal">
+          {Object.entries(ID_LABELS_MAPPING).map(([id, label]) => {
+            const active = id === activeId
+            return (
+              <li
+                onClick={(e) => {
+                  //e?.preventDefault()
+                  scrollToId(id)
+                }}
+                data-id={id}
+                className={cn(
+                  "flex items-center h-8 px-3 border-l-2 cursor-pointer duration-200",
+                  {
+                    "border-l-anakiwa-500 text-anakiwa-500 font-medium": active,
+                  }
+                )}
+              >
+                {label}
+              </li>
+            )
+          })}
         </ul>
       </div>
       {/* TODO: add lint to github for edit this page*/}
       <Link href={"/"}>
         <Button size="lg">
-          <span className="text-sm font-medium">Add resource</span>
+          <span className="text-sm font-medium text-left">Add resource</span>
         </Button>
       </Link>
     </div>
@@ -138,7 +157,7 @@ const ResourceNav = () => {
 export default function ResourcePage() {
   return (
     <div className="bg-second-gradient">
-      <div className="container grid grid-cols-1 px-4 md:grid-cols-[1fr_3fr_1fr] gap-8 py-10 lg:gap-16 md:pb-20">
+      <div className="container grid grid-cols-1 px-4 md:grid-cols-[1fr_3fr_1fr] gap-6 py-10 md:pb-20">
         <div className="hidden md:block"></div>
         <div>
           <div className="grid grid-cols-1 gap-16">
@@ -279,8 +298,8 @@ export default function ResourcePage() {
             </ResourceCard>
           </div>
         </div>
-        <div className="relative hidden right-10 md:block top-32">
-          <div className="sticky right-0 top-14">
+        <div className="relative right-0 z-0 hidden md:block top-32">
+          <div className="sticky right-0 ml-auto top-14">
             <ResourceNav />
           </div>
         </div>
