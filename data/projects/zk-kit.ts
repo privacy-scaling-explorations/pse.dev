650bbfc83a091387adfd912db7689dac99b57c97
Cedoor <me@cedoor.dev>
2023-12-05 15:00:04 +0000

    chore: update zk-kit project info

--- data/projects/zk-kit.ts
+++ data/projects/zk-kit.ts
@@ -3,14 +3,18 @@ import { ProjectInterface } from "@/lib/types";
 export const ZKKit: ProjectInterface = {
     id: "zk-kit",
     image: "zk-kit.svg",
-    name: "ZK Kit",
-    tldr: "A comprehensive monorepo of JS libraries for zero-knowledge technologies.",
-    description: "Designed to streamline the development process, it provides a set of NPM modules, including algorithms and utility functions, that can be integrated into various projects and zero-knowledge protocols. The toolkit is equipped with features like Yarn workspaces for package management, Conventional Commits for meaningful commit messages, Jest for testing, and Typedocs for TypeScript documentation. Developers can leverage these tools to ensure code quality, testing, and documentation without the overhead of setup.",
-    projectStatus: 'archived',
+    name: "ZK-kit",
+    tldr: "A monorepo of reusable libraries for zero-knowledge technologies.",
+    description: "ZK-kit is a set of libraries (plugins, algorithms or utility functions) that can be reused in different projects and zero-knowledge protocols, making it easier for developers to access user-friendly, tested, and documented libraries for common tasks.",
+    projectStatus: 'active',
     links: {
+        website: 'https://zkkit.pse.dev',
         github: 'https://github.com/privacy-scaling-explorations/zk-kit',
     },
     tags: {
-        keywords: ['anonymity']
+        keywords: ['Education', 'Toolkits', 'Anonymity/Privacy', 'Algorithms'],
+        themes: ["build"],
+        types: ["Legos/dev tools"],
+        builtWith: ["Circom", "JavaScript", "Solidity", "Noir"]
     }
 }
