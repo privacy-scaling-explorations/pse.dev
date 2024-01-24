9ebad52cd77bbbc04476ba739159de882ca6c968
Kalidou Diagne <kld.diagne@gmail.com>
2024-01-24 19:30:50 +0000

    header with container

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -11,6 +11,7 @@ import {
 
 import { Icons } from "./icons"
 import { SiteHeaderMobile } from "./site-header-mobile"
+import { AppContent } from "./ui/app-content"
 import { Dropdown } from "./ui/dropdown"
 
 type SiteHeaderProps = {
@@ -21,30 +22,32 @@ export function SiteHeader({ lang }: SiteHeaderProps) {
   const { MAIN_NAV } = useAppSettings(lang)
 
   return (
-    <header className="sticky top-0 z-40 w-full bg-white px-6 shadow-sm xl:px-20">
-      <div className="flex h-16 items-center justify-between space-x-4 sm:space-x-0">
-        <MainNav items={MAIN_NAV} lang={lang} />
-        <SiteHeaderMobile lang={lang} />
-        {siteConfig?.showLanguageSwitcher && (
-          <div className="hidden outline-none md:block">
-            <Dropdown
-              label={
-                <div className="flex items-center gap-1">
-                  <Icons.globe size={22} />
-                  <span className="!text-base !font-normal text-tuatara-950">
-                    {LanguageMapping[lang] ?? LanguageMapping["en"]}
-                  </span>
-                </div>
-              }
-              defaultItem={lang}
-              items={languagesItems}
-              onChange={(lang) => {
-                window?.location?.replace(`/${lang}`)
-              }}
-            />
-          </div>
-        )}
-      </div>
-    </header>
+    <AppContent>
+      <header className="sticky top-0 z-40 w-full bg-white shadow-sm">
+        <div className="flex h-16 items-center justify-between space-x-4 sm:space-x-0">
+          <MainNav items={MAIN_NAV} lang={lang} />
+          <SiteHeaderMobile lang={lang} />
+          {siteConfig?.showLanguageSwitcher && (
+            <div className="hidden outline-none md:block">
+              <Dropdown
+                label={
+                  <div className="flex items-center gap-1">
+                    <Icons.globe size={22} />
+                    <span className="!text-base !font-normal text-tuatara-950">
+                      {LanguageMapping[lang] ?? LanguageMapping["en"]}
+                    </span>
+                  </div>
+                }
+                defaultItem={lang}
+                items={languagesItems}
+                onChange={(lang) => {
+                  window?.location?.replace(`/${lang}`)
+                }}
+              />
+            </div>
+          )}
+        </div>
+      </header>
+    </AppContent>
   )
 }
