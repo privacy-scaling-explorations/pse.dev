8c75a7da6bea8a7ffc75808a992c6c5c174cfbfe
ChihChengLiang <chihchengliang@gmail.com>
2024-02-02 16:55:09 +0800

    lint ts

--- app/i18n/settings.ts
+++ app/i18n/settings.ts
@@ -1,7 +1,7 @@
 import type { InitOptions } from "i18next"
 
 export const fallbackLng = "en"
-export const languages = [fallbackLng, "it", "ko", "fr", "zh-CN",  "zh-TW", "es"] as const
+export const languages = [fallbackLng, "it", "ko", "fr", "zh-CN", "zh-TW", "es"] as const
 export type LocaleTypes = (typeof languages)[number]
 export const defaultNS = "translation"
 export const cookieName = "i18next"
