d416cd53472a338c90ba414a0efae49efd2649b6
Kalidou Diagne <kld.diagne@gmail.com>
2024-02-26 20:46:26 -0700

    about page new design

--- app/[lang]/about/page.tsx
+++ app/[lang]/about/page.tsx
@@ -3,37 +3,8 @@ import Image from "next/image"
 
 import { Accordion } from "@/components/ui/accordion"
 import { AppContent } from "@/components/ui/app-content"
+import { Label } from "@/components/ui/label"
 import { useTranslation } from "@/app/i18n"
-
-interface PrincipleContentProps {
-  image: string
-  children: React.ReactNode
-  width?: number
-  height?: number
-}
-
-const PrincipleContent = ({
-  image,
-  children,
-  width = 300,
-  height = 300,
-}: PrincipleContentProps) => {
-  return (
-    <div className="grid grid-cols-1 gap-6 py-4 md:mb-8 md:grid-cols-2 md:items-center md:gap-2 md:py-6">
-      <div className="m-auto py-6 md:py-0">
-        <Image
-          width={width}
-          height={height}
-          src={image}
-          alt="principle image"
-        />
-      </div>
-      <span className="flex flex-col gap-4 break-words font-sans text-lg font-normal leading-[150%]">
-        {children}
-      </span>
-    </div>
-  )
-}
 
 const PrincipleImageSizes: Record<string, { width: number; height: number }> = {
   "principle-1": {
@@ -63,55 +34,44 @@ export default async function AboutPage({ params: { lang } }: any) {
     }) ?? []
 
   return (
-    <div className="bg-anakiwa-200">
+    <div className="bg-anakiwa-50">
       <div className="bg-second-gradient">
-        <AppContent>
-          <div className="mx-auto grid grid-cols-1 gap-16 py-10 lg:grid-cols-[1fr_300px] lg:gap-2 lg:py-20">
-            <div className="flex flex-col gap-8 lg:w-4/5">
-              <h6 className="break-words font-display text-4xl font-bold text-tuatara-950 md:text-5xl">
-                {t("title")}
-              </h6>
+        <AppContent className="flex gap-[56px]">
+          <div className="grid w-full grid-cols-1 gap-16 py-10 lg:grid-cols-[1fr_1fr] lg:gap-2 lg:py-20">
+            <div className="flex w-full flex-col gap-8 lg:max-w-[650px]">
+              <Label.PageTitle label={t("title")} />
               <span className="font-sans text-base font-normal leading-[27px] text-tuatara-950">
                 {t("description")}
               </span>
             </div>
+            <Image
+              width={280}
+              height={280}
+              className="mx-auto h-[210px] w-[210px] lg:ml-auto lg:h-[320px] lg:w-[320px]"
+              src="/logos/pse-logo-bg.svg"
+              alt="pse logo"
+            />
           </div>
         </AppContent>
       </div>
 
       <AppContent className="flex flex-col gap-4 py-16 md:py-24">
-        <div className="mx-auto pb-4">
-          <Image
-            width={280}
-            height={280}
-            src="/logos/pse-logo-bg.svg"
-            alt="pse logo"
-          />
-        </div>
         <h6 className="font-display text-4xl">{t("our-principles-title")}</h6>
         <Accordion
           type="multiple"
           items={[
             ...principles?.map((principle: any, index: number) => {
-              const imageIndex = index + 1
-              const { width, height } =
-                PrincipleImageSizes[`principle-${imageIndex}`] ?? {}
-
               return {
                 label: principle?.title,
-                value: imageIndex.toString(),
+                value: index.toString(),
                 children: (
-                  <PrincipleContent
-                    width={width}
-                    height={height}
-                    image={`/logos/principle-${imageIndex}.svg`}
-                  >
+                  <span className="flex flex-col gap-4 break-words pb-12 font-sans text-lg font-normal leading-[150%]">
                     {principle.description?.map(
                       (description: string, index: number) => {
                         return <p key={index}>{description}</p>
                       }
                     )}
-                  </PrincipleContent>
+                  </span>
                 ),
               }
             }),
