69db95d22cf2806f867f46a8f14c4e7dd1529d38
Kalidou Diagne <kld.diagne@gmail.com>
2024-05-07 15:37:21 +0900

    fix url

--- app/[lang]/projects/[id]/page.tsx
+++ app/[lang]/projects/[id]/page.tsx
@@ -35,8 +35,7 @@ export async function generateMetadata(
   )[0]
 
   const WEBSITE_URL = process.env.VERCEL_URL || "https://pse.dev"
-  const BASE_URL =
-    process.env.NODE_ENV === "production" ? new URL(WEBSITE_URL) : "."
+  const BASE_URL = process.env.NODE_ENV === "production" ? WEBSITE_URL : "."
 
   const imageUrl =
     currProject?.image?.length > 0
