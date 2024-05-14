54eb26175fdcec7a761197b887756e7ddd90cab2
cedoor <me@cedoor.dev>
2024-05-14 16:41:34 +0100

    chore: update zk-kit logo and tagline

--- data/projects/zk-kit.ts
+++ data/projects/zk-kit.ts
@@ -4,10 +4,10 @@ export const ZKKit: ProjectInterface = {
   id: "zk-kit",
   section: "pse",
   image: "zk-kit.svg",
-  name: "ZK-kit",
-  tldr: "A monorepo of reusable libraries for zero-knowledge technologies.",
+  name: "ZK-Kit",
+  tldr: "A set of reusable libraries for zero-knowledge technologies.",
   description:
-    "ZK-kit is a set of libraries (plugins, algorithms or utility functions) that can be reused in different projects and zero-knowledge protocols, making it easier for developers to access user-friendly, tested, and documented libraries.",
+    "ZK-kit is a set of libraries (algorithms or utility functions) that can be reused in different projects and zero-knowledge protocols, making it easier for developers to access user-friendly, tested, and documented libraries.",
   projectStatus: "active",
   links: {
     website: "https://zkkit.pse.dev",
