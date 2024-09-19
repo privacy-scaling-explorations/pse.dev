c7602b7fe417b17197ae4a5c50f6182c04591190
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2024-09-19 08:26:12 +0200

    More TLSNotary info

--- data/projects/tlsn.ts
+++ data/projects/tlsn.ts
@@ -7,9 +7,10 @@ export const tlsn: ProjectInterface = {
   image: "tlsn.webp",
   name: "TLSNotary",
   links: {
-    github: "https://github.com/tlsnotary/tlsn",
+    github: "https://github.com/tlsnotary",
     website: "https://tlsnotary.org/",
     discord: "https://discord.gg/9XwESXtcN7",
+    twitter: "https://x.com/tlsnotary",
   },
   tags: {
     themes: ["build", "play"],
@@ -25,6 +26,20 @@ export const tlsn: ProjectInterface = {
       "Identity",
       "Reputation",
       "Data portability",
+    ],
+  },
+  extraLinks: {
+    play: [
+      {
+        label: "Getting started",
+        url: "https://docs.tlsnotary.org/quick_start/index.html",
+      },
+    ],
+    learn: [
+      {
+        label: "Documentation",
+        url: "https://docs.tlsnotary.org",
+      },
     ],
   },
 }
