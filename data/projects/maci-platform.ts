87194937ab5e971a541972ff69d60344604c5eca
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2024-11-05 00:49:37 +0900

    chore: update maci and maci platform descriptions

--- data/projects/maci-platform.ts
+++ data/projects/maci-platform.ts
@@ -3,7 +3,20 @@ import { ProjectContent, ProjectInterface, ProjectStatus } from "@/lib/types"
 const content: ProjectContent = {
   en: {
     tldr: "Run a voting round in your community! Forked from EasyRetroPGF and enhanced with MACI for privacy, anti-bribery and anti-collusion",
-    description: `MACI Platform is an implementation of EasyRetroPGF with MACI. This project enables any community, organization or ecosystem to run a voting round. It is highly configurable to enable different voting mechanisms (quadratic voting, quadratic funding, ranked choice, etc), gating mechanisms (token based, attestation based, hats-based, etc) and an easy-to-use UI for both round organizers and voters.`,
+    description: `### Overview
+
+[MACI Platform](https://github.com/privacy-scaling-explorations/maci-platform/tree/main) is an implementation of EasyRetroPGF with MACI. 
+
+This project enables any community, organization or ecosystem to run a voting round.
+It is highly configurable to enable different voting mechanisms (quadratic voting, quadratic funding, etc), gating mechanisms (token based, attestation based, hats-based, etc) and an easy-to-use UI for both round organizers and voters.
+
+### Features
+
+With MACI Platform, you can run different kind of voting rounds for your community, with the following features:
+
+1. Full benefit of MACI's properties - Plug into MACI's anti-collusion and privacy features
+2. Clean UX - The interface is optimised for voters and projects that want to participate in rounds
+3. Customisable - The platform can be customised quickly to fit different use cases`,
   },
 }
 
--- data/projects/maci.ts
+++ data/projects/maci.ts
@@ -3,7 +3,25 @@ import { ProjectContent, ProjectInterface, ProjectStatus } from "@/lib/types"
 const content: ProjectContent = {
   en: {
     tldr: "An on-chain voting solution that protects privacy and minimizes the risk of collusion and bribery",
-    description: `Minimal Anti-Collusion Infrastructure (MACI) is a protocol designed to provide a highly secure e-voting solution. It enables organizations to conduct on-chain voting processes with a significantly reduced risk of cheating, such as bribery or collusion. MACI uses zero-knowledge proofs to implement a receipt-free voting scheme, making it impossible for anyone other than the vote coordinator to verify how a specific user voted. This ensures the correct execution of votes and allows anyone to verify the results. It's particularly beneficial for governance and funding events, where its anti-collusion mechanisms help ensure fair and transparent outcomes.`,
+    description: `### Overview
+
+[MACI](https://github.com/privacy-scaling-explorations/maci/tree/dev) is a protocol designed to provide a highly secure e-voting solution. 
+
+It enables organisations to conduct on-chain voting processes with a significantly reduced risk of cheating, such as bribery or collusion. 
+
+MACI uses zero-knowledge proofs to implement a receipt-free voting scheme, making it impossible for anyone other than the vote coordinator to verify how a specific user voted. This ensures the correct execution of votes and allows anyone to verify the results. 
+
+It's particularly beneficial for governance and funding events, where its anti-collusion mechanisms help ensure fair and transparent outcomes. 
+
+Please refer to MACI's [documentation](https://maci.pse.dev) for more details.
+
+### Features
+
+With MACI, you can take on chain voting to the next level:
+
+1. Uncensorable - no one can censor votes, not even the coordinator
+2. Votes are private - votes are sent encrypted on chain and only the voter and the coordinator can decrypt them
+3. Fully verifiable voting process - anyone can verify the tally is correct thanks to zero-knowledge proofs`,
   },
 }
 
@@ -23,6 +41,6 @@ export const maci: ProjectInterface = {
     keywords: ["Anonymity/privacy", "Voting/governance"],
     themes: ["build"],
     types: ["Lego sets/toolkits", "Infrastructure/protocol"],
-    builtWith: ["p0tion", "JubjubLib"],
+    builtWith: ["p0tion", "zk-kit", "snarkjs", "circom", "solidity"],
   },
 }
