5445481d0fd9a365571a70a5b0ab566e67d8422c
NicoSerranoP <nnicosp@hotmail.com>
2024-12-04 18:08:26 -0500

    feat: update trusted setups projects status

--- data/projects/p0tion.ts
+++ data/projects/p0tion.ts
@@ -8,14 +8,24 @@ import {
 const content: ProjectContent = {
   en: {
     tldr: "Toolkit for Groth16 Phase 2 Trusted Setup ceremonies.",
-    description: `p0tion is an agnostic-from-ceremony public good toolkit, with the aim of making Groth16 zk-applications scale and become production-ready in a safe and secure manner by running Phase 2 Trusted Setup ceremonies.`,
+    description: `### Overview
+    p0tion is an agnostic-from-ceremony public good toolkit, with the aim of making Groth16 zk-applications scale and become production-ready in a safe and secure manner by running Phase 2 Trusted Setup ceremonies.
+
+    ### Status
+    This project has been sunset and it is entering to a long term support phase (no further development). p0tion toolkit is availabe for developers to use and run their own trusted setups ceremonies. We encourage the community to fork and continue the improvement of this toolkit.
+
+    ### Resources
+
+    - **p0tion**: A tool to help developers create and manage trusted setups - [GitHub](https://github.com/privacy-scaling-explorations/p0tion)
+    - **DefinitelySetup WebApp**: A WebApp to monitor and contribute to trusted setups - [Website](https://ceremony.pse.dev/) | [GitHub](https://github.com/privacy-scaling-explorations/DefinitelySetup)
+    `,
   },
 }
 
 export const p0tion: ProjectInterface = {
   id: "p0tion",
   category: ProjectCategory.DEVTOOLS,
-  projectStatus: ProjectStatus.ACTIVE,
+  projectStatus: ProjectStatus.INACTIVE,
   section: "pse",
   content,
   image: "p0tion.png",
