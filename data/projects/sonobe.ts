029d1aa5efe21263f88ae30c12c2a7caf04c2d78
0xalizk <42751398+0xalizk@users.noreply.github.com>
2024-12-10 20:55:33 +0300

    Update sonobe.ts

--- data/projects/sonobe.ts
+++ data/projects/sonobe.ts
@@ -8,16 +8,16 @@ import {
   const content: ProjectContent = {
     en: {
       tldr: "Modular library supporting multiple folding schemes and decider backends",
-      description: `[Sonobe](https://github.com/privacy-scaling-explorations/sonobe) is a modular library to fold arithmetic circuit instances in an Incremental Verifiable computation (IVC) style. It supports multiple frontends, multiple folding schemes, and multiple decider backends, allowing users to plug-and-play different components.\
-\
-The project is a collaboration between PSE and [0xPARC](https://0xparc.org).\
-\
+      description: `[Sonobe](https://github.com/privacy-scaling-explorations/sonobe) is a modular library to fold arithmetic circuit instances in an Incremental Verifiable computation (IVC) style. It supports multiple frontends, multiple folding schemes, and multiple decider backends, allowing users to plug-and-play different components.  
+  
+The project is a collaboration between PSE and [0xPARC](https://0xparc.org).  
+  
 **Frontends:** Arkworks, Circom, Noir and Noname.
-\
-**Folding schemes:** [Nova](https://eprint.iacr.org/2021/370), [CycleFold](https://eprint.iacr.org/2023/1192), [HyperNova](https://eprint.iacr.org/2023/573), and [ProtoGalaxy](https://eprint.iacr.org/2023/1106)
-\
-**Decider backends:** Groth16 and KZG commitment [proofs](https://privacy-scaling-explorations.github.io/sonobe-docs/usage/decider-prove.html) which are [verifiable on-chain](https://privacy-scaling-explorations.github.io/sonobe-docs/usage/solidity-verifier.html).
-\
+  
+**Folding schemes:** [Nova](https://eprint.iacr.org/2021/370), [CycleFold](https://eprint.iacr.org/2023/1192), [HyperNova](https://eprint.iacr.org/2023/573), and [ProtoGalaxy](https://eprint.iacr.org/2023/1106).  
+  
+**Decider backends:** Groth16 and KZG commitment [proofs](https://privacy-scaling-explorations.github.io/sonobe-docs/usage/decider-prove.html) which are [verifiable on-chain](https://privacy-scaling-explorations.github.io/sonobe-docs/usage/solidity-verifier.html).  
+  
 **Other features:** [ZK Layer](https://privacy-scaling-explorations.github.io/sonobe-docs/usage/nova-zk.html) and in-browser [WASM usage](https://privacy-scaling-explorations.github.io/sonobe-docs/usage/wasm.html).`,
     },
   }
