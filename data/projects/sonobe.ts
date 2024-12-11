462436b072e88923d9747ff7e2a36c2855f8e7e4
0xalizk <42751398+0xalizk@users.noreply.github.com>
2024-12-11 13:22:47 +0300

    Update sonobe.ts

--- data/projects/sonobe.ts
+++ data/projects/sonobe.ts
@@ -7,7 +7,7 @@ import {
   
   const content: ProjectContent = {
     en: {
-      tldr: "Modular library supporting multiple folding schemes and decider backends",
+      tldr: "Modular folding library supporting multiple schemes and decider backends",
       description: `In collaboration with [0xPARC](https://0xparc.org), [Sonobe](https://github.com/privacy-scaling-explorations/sonobe) is a modular library to fold arithmetic circuit instances in an Incremental Verifiable computation (IVC) style. It supports multiple frontends, multiple folding schemes, and multiple decider backends, allowing users to plug-and-play different components.    
   
 - **Frontends:** 
