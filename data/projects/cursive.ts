041e20e086e2029d08f159fecaba71d1d1df41bc
Kalidou Diagne <kld.diagne@gmail.com>
2024-12-19 00:54:11 +0100

    website updates (#253)

---
+++ data/projects/cursive.ts
@@ -0,0 +1,54 @@
+import {
+  ProjectCategory,
+  ProjectContent,
+  ProjectInterface,
+  ProjectStatus,
+} from "@/lib/types"
+
+const content: ProjectContent = {
+  en: {
+    tldr: "This project was built to activate NFCs at SBC and FtC and learn from it for a larger Devconnect experience with 200 cards and 5,000 attendees.",
+    description: `
+What began as an experimental initiative to transform PSE's theoretical cryptography research into tangible applications has evolved into a dedicated lab that incubates practical ways to connect people through applied cryptography.
+
+Cursive's work creates essential feedback loops between researchers and developers, continuously improving a variety of open source codebases. They also experiment with new interfaces to experience cryptography, finding particular success with NFC technology. They have used a variety of form factors like cards, stickers, and bracelets to enable users to securely collect private data for cryptographic computations. 
+
+While initial experiments focused on creating engaging ways for users to gather data for zero-knowledge proofs, Cursive is now expanding into developing meaningful multiparty computation (MPC) interactions that put cutting-edge cryptographic research directly into users' hands.
+
+Here are some product + research highlights:
+- Client-side Nova folding to provide a "Spotify Wrapped" summary of your ZKSummit11 experience: https://github.com/cursive-team/zk-summit
+- Private set intersection using multi-party FHE: https://github.com/cursive-team/2P-PSI
+- Private hiring matcher using phantom-zone, a full multi-party FHE circuit builder: https://github.com/cursive-team/pz-hiring
+- Trinity, a new 2PC scheme with optimal DevX and UX: https://github.com/cursive-team/trinity-v0
+`,
+  },
+}
+
+export const nfctap: ProjectInterface = {
+  id: "cursive",
+  projectStatus: ProjectStatus.ACTIVE,
+  category: ProjectCategory.APPLICATION,
+  section: "grant",
+  content,
+  image: "cursive.webp",
+  name: "Cursive",
+  links: {
+    github: "https://pse.dev/en/projects/cursive",
+    website: "https://www.cursive.team/",
+  },
+  tags: {
+    keywords: [
+      "anonymity/privacy",
+      "education",
+      "data portability",
+      "social",
+      "wallets",
+      "identity",
+      "key management",
+      "reputation",
+      "toolkits",
+    ],
+    builtWith: [],
+    themes: ["build", "play"],
+  },
+}
