ae9a99d30cb654c811fbef320374340bb08f4d4c
0xalizk <42751398+0xalizk@users.noreply.github.com>
2025-01-28 02:50:06 +0300

    Update data/projects/pse-halo2.ts
    Co-authored-by: Sam Richards <sbrichards@gmail.com>

--- data/projects/pse-halo2.ts
+++ data/projects/pse-halo2.ts
@@ -11,7 +11,7 @@ export const pse_halo2: ProjectInterface = {
       tldr: "PSE-Halo2 is a rearchiteted, KZG-backended fork of zcash's Halo2, an instantiation of PLONK, with support for additional curves among other new features",
       description: `PSE-Halo2 modified, extended, and eventually re-architected the original Halo2 implementation by zcash -an instantiation of the PLONK proof system. 
       
-      The original IPA backend was swapped with KZG for cost-effective L1 verifiability, and comes with a Solidity verifier. New features such as support for many additional curves were added, and the system was eventually re-architected by [splitting the front- and backends](https://github.com/privacy-scaling-explorations/halo2/pull/254). PSE-Halo has significant contributions from the wider community.`,
+      The original IPA backend was swapped with KZG for cost-effective L1 verifiability, and comes with a Solidity verifier. New features such as support for many additional curves were added, and the system was eventually re-architected by [splitting the front- and backends](https://github.com/privacy-scaling-explorations/halo2/pull/254). PSE-Halo2 has significant contributions from the wider community.`,
     },
   },
   tags: {
