f4e332cb86f7dd03e5c690f82034eba071036e19
Kalidou Diagne <kld.diagne@gmail.com>
2025-01-29 22:50:04 +0000

    fix markdown formatting

--- data/projects/powers-of-tau.ts
+++ data/projects/powers-of-tau.ts
@@ -8,12 +8,13 @@ import {
 const content: ProjectContent = {
   en: {
     tldr: "An ongoing (since 2019) zk-SNARK trusted setup ceremony for circuits up to 2^28 constraints.",
-    description: `### Overview
+    description: `
+### Overview
 
-    The Perpetual Powers of Tau is a multi-party trusted setup ceremony, rooted in the Zcash Powers of Tau. Its primary objective is the secure generation of zk-SNARK parameters for circuits accommodating up to 2^28 (260+ million) constraints. This translates to the creation of over 530 million powers of tau. The ceremony's integrity hinges on the principle that as long as one participant acts honestly and remains uncompromised, the entire setup remains trustworthy. It's a pivotal step for zk-SNARK projects, ensuring the security and privacy of the system. Those who can handle a 100Gb download and many hours of compute time are invited to join by contacting [@nicoserranop on Twitter](https://twitter.com/NicoSerranoP) or Telegram, or asking questions via the PSE [Discord](https://discord.com/invite/sF5CT5rzrR).
+The Perpetual Powers of Tau is a multi-party trusted setup ceremony, rooted in the Zcash Powers of Tau. Its primary objective is the secure generation of zk-SNARK parameters for circuits accommodating up to 2^28 (260+ million) constraints. This translates to the creation of over 530 million powers of tau. The ceremony's integrity hinges on the principle that as long as one participant acts honestly and remains uncompromised, the entire setup remains trustworthy. It's a pivotal step for zk-SNARK projects, ensuring the security and privacy of the system. Those who can handle a 100Gb download and many hours of compute time are invited to join by contacting [@nicoserranop on Twitter](https://twitter.com/NicoSerranoP) or Telegram, or asking questions via the PSE [Discord](https://discord.com/invite/sF5CT5rzrR).
 
-    ### Status
-    This project is entering a long term support phase. The ceremony is ongoing and accepting new participants. There are no further development plans for the project.
+### Status
+This project is entering a long term support phase. The ceremony is ongoing and accepting new participants. There are no further development plans for the project.
     `,
   },
 }
