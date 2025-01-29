da99c8b14e48330cba211766b0e1756fd9bd1027
Kalidou Diagne <kld.diagne@gmail.com>
2025-01-29 22:50:35 +0000

    fix markdown formatting (#263)

--- data/projects/trusted-setups.ts
+++ data/projects/trusted-setups.ts
@@ -8,18 +8,19 @@ import {
 const content: ProjectContent = {
   en: {
     tldr: "Aiding developers with tools for trusted setups.",
-    description: `### Overview
-    The Trusted Setups project is dedicated to simplifying the process of trusted setups, which are crucial for privacy or scaling solutions. Trusted setups involve multiple participants contributing to the generation of secrets. As long as one participant forgets their part of the secret, the final solution remains secure. The team recognizes the complexity of developing contribution programs and coordinating the participants' queue in a trusted setup. To address this, they are developing tools, including scripts, WebApps, and APIs, to streamline the contribution and coordination effort. This allows developers to focus on building their circuits and applications, enhancing efficiency and productivity in the development of zero-knowledge applications.
+    description: `
+### Overview
+The Trusted Setups project is dedicated to simplifying the process of trusted setups, which are crucial for privacy or scaling solutions. Trusted setups involve multiple participants contributing to the generation of secrets. As long as one participant forgets their part of the secret, the final solution remains secure. The team recognizes the complexity of developing contribution programs and coordinating the participants' queue in a trusted setup. To address this, they are developing tools, including scripts, WebApps, and APIs, to streamline the contribution and coordination effort. This allows developers to focus on building their circuits and applications, enhancing efficiency and productivity in the development of zero-knowledge applications.
 
-    ### Status
-    This project has been sunset and there will be no further development. The tools and resources developed are available under no warranty and are provided as-is. We believe they are enough for developers to run their trusted setups on their own. The team encourages the community to fork and continue the development of these tools.
+### Status
+This project has been sunset and there will be no further development. The tools and resources developed are available under no warranty and are provided as-is. We believe they are enough for developers to run their trusted setups on their own. The team encourages the community to fork and continue the development of these tools.
 
-    ### Resources
+### Resources
 
-    - **p0tion**: A tool to help developers create and manage trusted setups - [GitHub](https://github.com/privacy-scaling-explorations/p0tion)
-    - **DefinitelySetup WebApp**: A WebApp to monitor and contribute to trusted setups - [Website](https://ceremony.pse.dev/) | [GitHub](https://github.com/privacy-scaling-explorations/DefinitelySetup)
-    - **KZG WebApp**: A WebApp used to contributed to the KZG Ceremony - [Website](https://ceremony.ethereum.org/) | [GitHub](https://github.com/zkparty/trusted-setup-frontend)
-    - **Perpetual Powers of Tau**: A phase 1 ceremony to generate Powers of Tau - [GitHub](https://github.com/privacy-scaling-explorations/perpetualpowersoftau)
+- **p0tion**: A tool to help developers create and manage trusted setups - [GitHub](https://github.com/privacy-scaling-explorations/p0tion)
+- **DefinitelySetup WebApp**: A WebApp to monitor and contribute to trusted setups - [Website](https://ceremony.pse.dev/) | [GitHub](https://github.com/privacy-scaling-explorations/DefinitelySetup)
+- **KZG WebApp**: A WebApp used to contributed to the KZG Ceremony - [Website](https://ceremony.ethereum.org/) | [GitHub](https://github.com/zkparty/trusted-setup-frontend)
+- **Perpetual Powers of Tau**: A phase 1 ceremony to generate Powers of Tau - [GitHub](https://github.com/privacy-scaling-explorations/perpetualpowersoftau)
     `,
   },
 }
