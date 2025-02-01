6a64673795b63e7a115ff8fc7df67bd981808094
Sam Richards <sbrichards@gmail.com>
2025-02-01 16:20:14 -0300

    update tldr without markdown [fixes #270]

--- data/projects/pse-halo2.ts
+++ data/projects/pse-halo2.ts
@@ -11,13 +11,11 @@ export const pse_halo2: ProjectInterface = {
   },
   content: {
     en: {
-      tldr: `
-[PSE-Halo2](https://github.com/privacy-scaling-explorations/halo2) is a rearchiteted, KZG-backended fork of zcash's Halo2, an instantiation of PLONK, with support for [more curves](https://github.com/privacy-scaling-explorations/halo2curves) and other [experimental features](https://github.com/kilic/tetris)
-`,
+      tldr: "PSE-Halo2 is a re-architected, KZG-backended fork of Zcash's Halo2, an instantiation of PLONK, with support for more curves and other features.",
       description: `
-PSE-Halo2 modified, extended, and eventually [re-architected](https://github.com/privacy-scaling-explorations/halo2/pull/254) the original Halo2 implementation by zcash -an instantiation of the PLONK proof system. 
+PSE-Halo2 modified, extended, and eventually [re-architected](https://github.com/privacy-scaling-explorations/halo2/pull/254) the [original Halo2 implementation by Zcash](https://github.com/zcash/halo2) - an instantiation of the PLONK proof system.
       
-The original IPA backend was swapped with KZG for cost-effective L1 verifiability, and comes with a Solidity verifier. New features such as support for many additional curves were added, and the system was eventually re-architected by [splitting the front- and backends](https://github.com/privacy-scaling-explorations/halo2/pull/254). PSE-Halo2 has significant contributions from the wider community.`,
+The original IPA backend was swapped with KZG for cost-effective L1 verifiability, and comes with a Solidity verifier. Support for many [additional curves](https://github.com/privacy-scaling-explorations/halo2curves) and other [experimental features](https://github.com/kilic/tetris) were added, and the system was eventually re-architected by [splitting the front- and backends](https://github.com/privacy-scaling-explorations/halo2/pull/254). PSE-Halo2 has significant contributions from the wider community.`,
     },
   },
   tags: {
