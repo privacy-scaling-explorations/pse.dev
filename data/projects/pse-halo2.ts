f46d659e11e1bc1a2de96d97912a9573ab80eac9
Sam Richards <sbrichards@gmail.com>
2025-02-03 05:54:25 -0800

    Update halo2 to maintenance (#271)
    * update halo2 to maintenance

--- data/projects/pse-halo2.ts
+++ data/projects/pse-halo2.ts
@@ -5,19 +5,18 @@ export const pse_halo2: ProjectInterface = {
   image: "",
   name: "PSE-Halo2",
   section: "pse",
-  projectStatus: ProjectStatus.INACTIVE,
+  projectStatus: ProjectStatus.MAINTAINED,
   links: {
     github: "https://github.com/privacy-scaling-explorations/halo2",
+    website: "https://privacy-scaling-explorations.github.io/halo2/",
   },
   content: {
     en: {
-      tldr: `
-[PSE-Halo2](https://github.com/privacy-scaling-explorations/halo2) is a rearchiteted, KZG-backended fork of zcash's Halo2, an instantiation of PLONK, with support for [more curves](https://github.com/privacy-scaling-explorations/halo2curves) and other [experimental features](https://github.com/kilic/tetris)
-`,
+      tldr: "PSE-Halo2 is a re-architected, KZG-backended fork of Zcash's Halo2, an instantiation of PLONK, with support for more curves and other features.",
       description: `
-PSE-Halo2 modified, extended, and eventually [re-architected](https://github.com/privacy-scaling-explorations/halo2/pull/254) the original Halo2 implementation by zcash -an instantiation of the PLONK proof system. 
+PSE-Halo2 modified, extended, and eventually [re-architected](https://github.com/privacy-scaling-explorations/halo2/pull/254) the [original Halo2 implementation by Zcash](https://github.com/zcash/halo2) - an instantiation of the PLONK proof system.
       
-The original IPA backend was swapped with KZG for cost-effective L1 verifiability, and comes with a Solidity verifier. New features such as support for many additional curves were added, and the system was eventually re-architected by [splitting the front- and backends](https://github.com/privacy-scaling-explorations/halo2/pull/254). PSE-Halo2 has significant contributions from the wider community.`,
+The original IPA backend was swapped with KZG for cost-effective L1 verifiability, and comes with a Solidity verifier. Support for many [additional curves](https://github.com/privacy-scaling-explorations/halo2curves) and other [experimental features](https://github.com/kilic/tetris) were added, and the system was eventually re-architected by [splitting the front- and backends](https://github.com/privacy-scaling-explorations/halo2/pull/254). PSE-Halo2 has significant contributions from the wider community.`,
     },
   },
   tags: {
@@ -25,5 +24,13 @@ The original IPA backend was swapped with KZG for cost-effective L1 verifiabilit
     themes: ["build"],
     types: ["Infrastructure/protocol", "Lego sets/toolkits"],
     builtWith: ["halo2", "rust"],
+  },
+  extraLinks: {
+    learn: [
+      {
+        label: "Documentation",
+        url: "https://privacy-scaling-explorations.github.io/halo2/",
+      },
+    ],
   },
 }
