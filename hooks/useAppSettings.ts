3dbdd91e000c8a79c2e9ab622acaa8f04cf00315
Kalidou Diagne <kld.diagne@gmail.com>
2025-02-05 00:58:03 +0000

    Remove Devcon 7 link from top header

--- hooks/useAppSettings.ts
+++ hooks/useAppSettings.ts
@@ -1,44 +1,45 @@
-import { MainNavProps } from "@/components/main-nav"
-import { useTranslation } from "@/app/i18n/client"
-import { LocaleTypes, fallbackLng, languageList } from "@/app/i18n/settings"
+import { MainNavProps } from '@/components/main-nav'
+import { useTranslation } from '@/app/i18n/client'
+import { LocaleTypes, fallbackLng, languageList } from '@/app/i18n/settings'
 
 export function useAppSettings(lang: LocaleTypes) {
-  const { t } = useTranslation(lang, "common")
+  const { t } = useTranslation(lang, 'common')
 
   // get the active language label
   const activeLanguage =
     languageList.find((language) => language.key === lang)?.label ??
     languageList.find((language) => language.key === fallbackLng)?.label
 
-  const MAIN_NAV: MainNavProps["items"] = [
+  const MAIN_NAV: MainNavProps['items'] = [
     {
-      title: t("menu.home"),
-      href: "/",
+      title: t('menu.home'),
+      href: '/',
       onlyMobile: true,
     },
     {
-      title: "Devcon 7",
-      href: "/devcon-7",
+      title: 'Devcon 7',
+      href: '/devcon-7',
+      onlyFooter: true,
     },
     {
-      title: t("menu.projectLibrary"),
-      href: "/projects",
+      title: t('menu.projectLibrary'),
+      href: '/projects',
     },
     {
-      title: t("menu.programs"),
-      href: "/programs",
+      title: t('menu.programs'),
+      href: '/programs',
     },
     {
-      title: t("menu.about"),
-      href: "/about",
+      title: t('menu.about'),
+      href: '/about',
     },
     {
-      title: t("menu.resources"),
-      href: "/resources",
+      title: t('menu.resources'),
+      href: '/resources',
     },
     {
-      title: t("menu.blog"),
-      href: "https://mirror.xyz/privacy-scaling-explorations.eth",
+      title: t('menu.blog'),
+      href: 'https://mirror.xyz/privacy-scaling-explorations.eth',
       external: true,
       onlyHeader: true,
     },
