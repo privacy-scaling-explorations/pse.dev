47edfb6dadbe46b3dea3c3c681518996bd70a5cb
Kalidou Diagne <kld.diagne@gmail.com>
2025-08-01 21:50:33 +0200

    Update rss.ts

--- lib/rss.ts
+++ lib/rss.ts
@@ -89,7 +89,7 @@ export async function generateRssFeed() {
     // Add articles to feed
     articles.forEach((article) => {
       try {
-        const url = `${SITE_URL}/articles/${article.slug}`
+        const url = `${SITE_URL}/blog/${article.slug}`
         const pubDate = formatDate(article.frontmatter.date)
 
         feed.addItem({
